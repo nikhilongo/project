574cb653df56cc75a7b73f49fda39c1234b92ec0
nikhilongo <nikhilgupta8235@gmail.com>
2025-11-22 16:22:24 +0530

    Remove myenv from repo and update .gitignore

--- app.py
+++ app.py
@@ -5,17 +5,15 @@ import pandas as pd
 from sklearn.preprocessing import StandardScaler
 from src.pipeline.predict_pipeline import CustomData,PredictPipeline
 
-application=Flask(__name__)
-
-app=application
+app=Flask(__name__)
 
 ## Route for a home page
 
-@app.route('/')
-def index():
-    return render_template('index.html') 
+# @app.route('/')
+# def index():
+#     return render_template('index.html') 
 
-@app.route('/predictdata',methods=['GET','POST'])
+@app.route('/',methods=['GET','POST'])
 def predict_datapoint():
     if request.method=='GET':
         return render_template('home.html')
@@ -42,4 +40,4 @@ def predict_datapoint():
     
 
 if __name__=="__main__":
-    app.run(host="0.0.0.0",debug=True)        
+    app.run(host="0.0.0.0",port=5000)        
